de0ceefade5ff3b1667142f8e018fcd5f3668373
yamato <yamatosakino1113@icloud.com>
2021-08-09 21:17:37 +0900

    readme

--- hrcpy/test.py
+++ hrcpy/test.py
@@ -8,7 +8,7 @@ import numpy as np
 if  __name__ == '__main__':
 #==============================================================#
 #------------ INPUT TEST --------------#
-    """
+    
     data = Data.sinwave(L=50,X=2,Y=2000)
     #print(data)
     Input= Input(N_u = 2,
@@ -20,7 +20,7 @@ if  __name__ == '__main__':
 
     x = list(range(10000))
     plt.plot(x,u_s[0,:10000])
-    plt.show()"""
+    plt.show()
 #==============================================================#
 #------------ RESERVOIR TEST ------------#
     reservoir = Reservoir(
@@ -34,6 +34,7 @@ if  __name__ == '__main__':
 
     W = reservoir.make_connection()
     pprint(W)
+    
 
 #==============================================================#
 #------------ OUTPUT TEST ------------#
