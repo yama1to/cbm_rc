7c75ac9aac637f3ae0a0c8bf3cbdbacfe562519b
yama1to <yamatosakino1113@gmail.com>
2021-08-30 14:42:48 +0900

    change

--- generate_data_sequence_speech.py
+++ generate_data_sequence_speech.py
@@ -13,22 +13,6 @@ import copy
 
 from lyon.calc import LyonCalc
 
-"""def audio(wf):
-    p = pyaudio.PyAudio()
-    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
-                    channels=wf.getnchannels(),
-                    rate=wf.getframerate(),
-                    output=True) # このストリームに書き込むと音がなる
-
-    chunk = 1024 # チャンク単位でストリームに出力し音声を再生
-    wf.rewind() # ポインタを先頭にする
-    data = wf.readframes(chunk)
-    while data:
-        stream.write(data)
-        data = wf.readframes(chunk)
-    stream.close()
-    p.terminate()
-"""
 def generate_coch():
     np.random.seed(seed=0)
 
@@ -52,8 +36,6 @@ def generate_coch():
 
     train = np.array(train)
     valid = np.array(valid)
-
-
     train_data = np.zeros((250,12500))
     valid_data = np.zeros((250,12500))
 
@@ -101,68 +83,42 @@ def generate_coch():
                 x1+= 1
 
 
-    #print(train_data.shape)
-    #print(valid_data.shape)
-    """
-    train_data (250,12500) #(データ数,データ) データ数 = 0x25,1x25,2x25,...,9x25
-    valid_data (250,12500) #(データ数,データ)
-
-    """
-
-    #"""
-
     calc = LyonCalc()
 
     waveform = train_data
     sample_rate = 12500
-    train_coch = np.zeros((250,195,55))
-    
+
+    shap = (250,195,86)
+    train_coch = np.zeros(shap)
     for i in range(250):
-        c = calc.lyon_passive_ear(waveform[i], sample_rate, decimation_factor=64, ear_q=8, step_factor=0.35, tau_factor=3)
+        c = calc.lyon_passive_ear(waveform[i], sample_rate, decimation_factor=64, ear_q=8, step_factor=0.226, tau_factor=3)
         train_coch[i] = c
 
     waveform = valid_data
-    valid_coch = np.zeros((250,195,55))
+
+    valid_coch = np.zeros(shap)
+
     for i in range(250):
-        c = calc.lyon_passive_ear(waveform[i], sample_rate, decimation_factor=86, ear_q=8, step_factor=0.35, tau_factor=3)
+        c = calc.lyon_passive_ear(waveform[i], sample_rate, decimation_factor=64, ear_q=8, step_factor=0.226, tau_factor=3)
         valid_coch[i] = c
     
-    #print(train_coch.shape,valid_coch.shape)
 
-    print(train_coch.shape)
     idx = np.random.randint(0,100)
-    coch = train_coch[idx]
-    print(idx)
-    print(coch.shape)
-    fig=plt.figure()
-    
-    ax = fig.add_subplot(2,1,1)
-    ax.cla()
-    ax.imshow(coch.T)
 
-    ax = fig.add_subplot(2,1,2)
-    ax.cla()
-    plt.plot(waveform[idx])
-    plt.xlim(0,waveform[idx].shape[0])
+    shap2 = (250,195,10)
+    collecting_target = np.zeros(shap2)
 
-    plt.savefig("test.png")
-    plt.show()
-    init = np.zeros((250,10))
-    train_target = init
-    train_target[:50][-1] = 1
-    
-    train_target[50:100][-2] = 1
-    train_target[100:150][-3] = 1
-    train_target[150:200][-5] = 1
-    train_target[200:250][-4] = 1
+    for i in range(shap[0]):
+        collecting_target[i,:,i//25] = 1
 
-    valid_target = train_target
-
-    return train_coch,valid_coch ,train_target, valid_target
+    #print(collecting_target)
+    return train_coch,valid_coch ,collecting_target, collecting_target
 
 if __name__ == "__main__":
+
     t,v,tD,vD = generate_coch()
-    print(t[0],v[10])
+    print(t.shape,v.shape,tD.shape,vD.shape)
+    """print(t[0],v[10])
     print(tD == vD)
     tD = tD-1
-    print(tD == vD)
+    print(tD == vD)"""
