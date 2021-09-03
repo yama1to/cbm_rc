0b38a6c5180a0c3c848117f22fc3faf36db3f172
yamato <yamatosakino1113@icloud.com>
2021-09-03 23:17:12 +0900

    new

---
+++ generate_data_sequence_narma.py
@@ -0,0 +1,32 @@
+from typing import Sequence
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_narma(N,seed=0):
+    np.random.seed(seed=seed)
+    """Generate NARMA sequence."""
+    
+    u = np.random.uniform(0,0.5,(N+200))
+
+    # Generate NARMA sequence
+    d = np.zeros((N+200))
+    for i in range(9, N+200-1):
+        d[i+1] = 0.3*d[i] + 0.05*d[i] * \
+            np.sum(d[i-9:i+1]) + 1.5*u[i-9]*u[i] + 0.1
+    
+    u = u[200:]
+    d = d[200:]
+    if np.isfinite(d).all():
+        u = u.reshape((N,1))
+        d = d.reshape((N,1))
+        return u,d
+    else:
+        print("again")
+        return generate_narma(seed=seed+1)
+
+if __name__ == '__main__':
+    u,d = generate_narma(1000)
+    plt.plot(d)
+    plt.show()
+    print(u.shape,d.shape)
