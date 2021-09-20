4eff3dfa0780241e18fa5a202014bc5b66eda851
yama1to <yamatosakino1113@gmail.com>
2021-09-21 08:34:46 +0900

    add

--- test_cbmrc9a_speech4.py
+++ test_cbmrc9a_speech4.py
@@ -16,15 +16,13 @@ from explorer import randomsearch as rs
 from explorer import optimization as opt
 
 ### 共通設定
-from esn_speech4 import Config
-
+from cbmrc9a_speech4 import Config
 config = Config()
 common.config  = config
-common.prefix  = "data%s_esn_speech4" % common.string_now() # 実験名（ファイルの接頭辞）
-common.dir_path= "data/data%s_esn_speech4" % common.string_now() # 実験データを出力するディレクトリのパス
-common.exe     = "python esn_speech4.py " # 実行されるプログラム
-common.columns =['dataset','seed','id','Nh','alpha_i','alpha_r','alpha0','beta_i','beta_r',
-'lambda0',"train_WER","WER"]
+common.prefix  = "data%s_cbmrc9a_speech4" % common.string_now() # 実験名（ファイルの接頭辞）
+common.dir_path= "data/data%s_cbmrc9a_speech4" % common.string_now() # 実験データを出力するディレクトリのパス
+common.exe     = "python cbmrc9a_speech4.py " # 実行されるプログラム
+common.columns =['dataset','seed','id','NN','Nh','alpha_i','alpha_r','alpha_b','alpha_s','beta_i','beta_r','beta_b','Temp','lambda0','cnt_overflow','WER']
 common.parallel= 24
 common.setup()
 common.report_common()
@@ -37,7 +35,7 @@ def rs1():
     rs.append("alpha_i",min=0,max=5)
     rs.random(num=60,samples=2)
     df = common.load_dataframe() # 直前に保存されたcsvファイルをデータフレーム(df)に読み込む
-    df = df[['alpha_r','alpha_i','train_WER','WER']] # 指定した列のみでデータフレームを構成する
+    df = df[['alpha_r','alpha_i','cnt_overflow','WER']] # 指定した列のみでデータフレームを構成する
     #df = df[(df['y1']<=10.0)] # 条件を満たすデータについてデータフレームを構成する。
     #print(df)
     scatter_matrix(df, alpha=0.8, figsize=(6, 6), diagonal='kde')
@@ -52,16 +50,15 @@ def optimize():
     opt.clear()#設定をクリアする
     opt.appendid()#id:必ず加える
     opt.appendseed()# 乱数のシード（０から始まる整数値）
-    #opt.append("Nh",value=500,min=300,max=1000,round=1)
+    # 変数の追加([変数名],[基本値],[下端],[上端],[まるめの桁数])
     opt.append("beta_r",value=0.01,min=0.01,max=1,round=2)
     opt.append("beta_i",value=0.01,min=0.01,max=1,round=2)
     #opt.append("alpha_i",value=1,min=1,max=10000,round=2)
     opt.append("alpha_r",value=0.9,min=0.7,max=1,round=2)
-    opt.append("alpha0",value=1,min=0.01,max=1,round=2)
-    opt.minimize(target="WER",iteration=50,population=20,samples=3)
+    opt.minimize(target="WER",iteration=10,population=20,samples=3)
     #opt.minimize(TARGET=func,iteration=5,population=10,samples=4)
     common.config = opt.best_config # 最適化で得られた設定を基本設定とする
-#optimize()
+optimize()
 
 def plot1(x,y,ystd,ymin,ymax,color=None,width=1,label=None):
     # エラーバーをつけてグラフを描画、平均、標準偏差、最大値、最小値をプロットする。
@@ -81,15 +78,15 @@ def gridsearch(X1,min=0,max=1,num=41,samples=10):
     plt.figure(figsize=(6,8))
 
     plt.subplot(2,1,1)
-    x,ymean,ystd,ymin,ymax = vs.analyze(df,X1,"WER")
-    plot1(x,ymean,ystd,ymin,ymax,color=cmap(1),label="WER")
-    plt.ylabel("WER")
+    x,ymean,ystd,ymin,ymax = vs.analyze(df,X1,"BER")
+    plot1(x,ymean,ystd,ymin,ymax,color=cmap(1),label="BER")
+    plt.ylabel("BER")
     plt.grid(linestyle="dotted")
 
     plt.subplot(2,1,2)
-    x,ymean,ystd,ymin,ymax = vs.analyze(df,X1,"train_WER")
-    plot1(x,ymean,ystd,ymin,ymax,color=cmap(2),label="train_WER")
-    plt.ylabel("train_WER")
+    x,ymean,ystd,ymin,ymax = vs.analyze(df,X1,"cnt_overflow")
+    plot1(x,ymean,ystd,ymin,ymax,color=cmap(2),label="cnt_overflow")
+    plt.ylabel("overflow")
     #plt.yscale('log')
     plt.grid(linestyle="dotted")
     #plt.ylim([0,1]) # y軸の範囲
@@ -99,11 +96,7 @@ def gridsearch(X1,min=0,max=1,num=41,samples=10):
 
 def gs2():
     ns=3
-    gridsearch("Nh",min=100,max=1000,num=41,samples=ns)
-    #gridsearch("")
-    # gridsearch("Temp",min=0.01,max=10,num=100,samples=ns)
-    # gridsearch("beta_i",min=0.05,max=1,num=30,samples=ns)
-    #gridsearch("alpha_r",min=0.05,max=1,num=30,samples=ns)
-    #gridsearch("alpha_i",min=1,max=20000,num=501,samples=ns)
-    
-gs2()
+    gridsearch("alpha_r",min=0.0,max=2,num=41,samples=ns)
+    gridsearch("alpha_i",min=0.0,max=1,num=41,samples=ns)
+    gridsearch("alpha_s",min=0.0,max=1,num=41,samples=ns)
+#gs2()
