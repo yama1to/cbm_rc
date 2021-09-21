7c834c9ffad867ddcfb25192c631db54daf59037
yama1to <yamatosakino1113@gmail.com>
2021-09-21 09:09:29 +0900

    add

--- test_cbmrc9a_speech4.py
+++ test_cbmrc9a_speech4.py
@@ -23,7 +23,7 @@ common.prefix  = "data%s_cbmrc9a_speech4" % common.string_now() # 実験名（�
 common.dir_path= "data/data%s_cbmrc9a_speech4" % common.string_now() # 実験データを出力するディレクトリのパス
 common.exe     = "python cbmrc9a_speech4.py " # 実行されるプログラム
 common.columns =['dataset','seed','id','NN','Nh','alpha_i','alpha_r','alpha_b','alpha_s','beta_i','beta_r','beta_b','Temp','lambda0','cnt_overflow','WER']
-common.parallel= 24
+common.parallel= 64
 common.setup()
 common.report_common()
 common.report_config(config)
@@ -51,10 +51,11 @@ def optimize():
     opt.appendid()#id:必ず加える
     opt.appendseed()# 乱数のシード（０から始まる整数値）
     # 変数の追加([変数名],[基本値],[下端],[上端],[まるめの桁数])
-    opt.append("beta_r",value=0.01,min=0.01,max=1,round=2)
-    opt.append("beta_i",value=0.01,min=0.01,max=1,round=2)
-    #opt.append("alpha_i",value=1,min=1,max=10000,round=2)
+    opt.append("beta_r",value=0.1,min=0.01,max=1,round=2)
+    opt.append("beta_i",value=0.9,min=0.01,max=1,round=2)
+    opt.append("alpha_i",value=400,min=1,max=10000,round=2)
     opt.append("alpha_r",value=0.9,min=0.7,max=1,round=2)
+    opt.append("alpha_s",value=1,min=0.1,max=10,round=2)
     opt.minimize(target="WER",iteration=10,population=20,samples=3)
     #opt.minimize(TARGET=func,iteration=5,population=10,samples=4)
     common.config = opt.best_config # 最適化で得られた設定を基本設定とする
@@ -96,7 +97,9 @@ def gridsearch(X1,min=0,max=1,num=41,samples=10):
 
 def gs2():
     ns=3
-    gridsearch("alpha_r",min=0.0,max=2,num=41,samples=ns)
-    gridsearch("alpha_i",min=0.0,max=1,num=41,samples=ns)
-    gridsearch("alpha_s",min=0.0,max=1,num=41,samples=ns)
-#gs2()
+    gridsearch("alpha_r",min=0.01,max=1,num=41,samples=ns)
+    gridsearch("alpha_i",min=0.01,max=10000,num=41,samples=ns)
+    gridsearch("alpha_s",min=0.01,max=10,num=41,samples=ns)
+    gridsearch("beta_i",min=0.01,max=1,num=41,samples=ns)
+    gridsearch("beta_r",min=0.01,max=1,num=41,samples=ns)
+gs2()
