2a7966677a3b6f6d43328c58d5c510564f602a0e
yama1to <yamatosakino1113@gmail.com>
2021-10-05 20:13:01 +0900

    ad

--- esn_narma.py
+++ esn_narma.py
@@ -1,74 +1,226 @@
+# Copyright (c) 2018-2021 Katori lab. All Rights Reserved
+"""
+NOTE: cbm_rc　時系列生成タスク　
+cbmrc6e.pyを改変
+Configクラスによるパラメータ設定
+"""
+
+import argparse
 import numpy as np
+import scipy.linalg
 import matplotlib.pyplot as plt
-from esn_model_text import ESN, Tikhonov
+import copy
+import time
+from explorer import common
+from generate_data_sequence_narma import *
+from generate_matrix import *
 
-from  generate_data_sequence_narma  import *
+class Config():
+    def __init__(self):
+        # columns, csv, id: データの管理のために必須の変数
+        self.columns = None # 結果をCSVに保存する際のコラム
+        self.csv = None # 結果を保存するファイル
+        self.id  = None
+        self.plot = True # 図の出力のオンオフ
+        self.show = False # 図の表示（plt.show()）のオンオフ、explorerは実行時にこれをオフにする。
+        self.savefig = False
+        self.fig1 = "fig1.png" ### 画像ファイル名
 
-if __name__ == '__main__':
+        # config
+        self.dataset=6
+        self.seed:int=2 # 乱数生成のためのシード
+        self.MM=500 # サイクル数
+        self.MM0 = 0 #
+
+        self.Nu =  1  #size of input
+        self.Nh:int = 300#815 #size of dynamical reservior
+        self.Ny = 1   #size of output
 
 
-    rm = 100
-    MM1 = 900
-    MM2 = 100
-    U,D  = generate_narma(N=MM1+MM2+rm,seed=1)
-    U = U[rm:]
-    D = D[rm:]
-    train_U = U[:MM1]
-    test_U = U[MM1:]
-    train_D = D[:MM1]
-    test_D = D[MM1:]
+        #sigma_np = -5
+        self.alpha_i = 0.9
+        self.alpha_r = 0.9
+        self.alpha_b = 0.
+
+        self.alpha0 = 1#0.1
+        self.alpha1 = 0#-5.8
+
+        self.beta_i = 0.01
+        self.beta_r = 0.01
+        self.beta_b = 0.1
+
+        self.lambda0 = 0.1
+
+        # Results
+        self.RMSE = None
+        self.NRMSE=None
+        self.NMSE=None
 
 
-    # ESNモデル
-    N_x = 100  # リザバーのノード数
-    model = ESN(train_U.shape[1], train_D.shape[1], N_x, 
-                density=0.15, input_scale=0.1, rho=0.9,
-                fb_scale=0.1, fb_seed=0)
 
-    # 学習（リッジ回帰）
-    train_Y = model.train(train_U, train_D, 
-                          Tikhonov(N_x, train_D.shape[1], 1e-4)) 
+def generate_weight_matrix():
+    global Wr, Wb, Wo, Wi
+    Wr = generate_random_matrix(c.Nh,c.Nh,c.alpha_r,c.beta_r,distribution="one",normalization="sr")
+    Wb = generate_random_matrix(c.Nh,c.Ny,c.alpha_b,c.beta_b,distribution="one",normalization="none")
+    Wi = generate_random_matrix(c.Nh,c.Nu,c.alpha_i,c.beta_i,distribution="one",normalization="none")
+    Wo = np.zeros(c.Nh * c.Ny).reshape((c.Ny, c.Nh))
 
-    # モデル出力
-    test_Y = model.predict(test_U)
+def fy(h):
+    return np.tanh(h)
 
-    # 評価（テスト誤差RMSE, NRMSE）
-    RMSE = np.sqrt(((test_D - test_Y) ** 2).mean())
-    NRMSE = RMSE/np.sqrt(np.var(test_D))
-    print('RMSE =', RMSE)
-    print('NRMSE =', NRMSE)
+def run_network(mode):
+    global Hp
+    
+    Hp = np.zeros((c.MM, c.Nh))
+    #x = np.random.uniform(-1, 1, Nh)/ 10**4
+    x = np.zeros(c.Nh)
+    
 
-    plt.plot(test_Y)
-    plt.plot(test_D)
+    for n in range(c.MM):
+        
+        u = Up[n, :]
+
+        #Hp[n+1,:] = x + 1.0/tau * (-alpha0 * x + fx(Wi@u + Wr@x))
+        next_x = (1 - c.alpha0) * x + c.alpha0*fy(Wi@u + Wr@x)
+        Hp[n,:] = next_x
+        x= next_x
+
+        
+
+
+def train_network():
+    global Wo
+
+    run_network(1) # run netwrok with teacher forcing
+
+    M = Hp[c.MM0:, :]
+    invD = Dp
+    G = invD[c.MM0:, :]
+
+    #print("Hp\n",Hp)
+    #print("M\n",M)
+
+    ### Ridge regression
+    if c.lambda0 == 0:
+        Wo = np.dot(G.T,np.linalg.pinv(M).T)
+        #print("a")
+    else:
+        E = np.identity(c.Nh)
+        TMP1 = np.linalg.inv(M.T@M + c.lambda0 * E)
+        WoT = TMP1@M.T@G
+        Wo = WoT.T
+
+    #print("WoT\n", WoT)
+
+def test_network():
+    global Yp
+    run_network(0)
+
+    YpT = Wo @ Hp.T
+    Yp = YpT.T
+
+
+
+def plot1():
+    fig=plt.figure(figsize=(20, 12))
+    Nr=3
+    ax = fig.add_subplot(Nr,1,1)
+    ax.cla()
+    ax.set_title("U")
+    ax.plot(Up)
+
+    ax = fig.add_subplot(Nr,1,2)
+    ax.cla()
+    ax.set_title("X")
+    ax.plot(Hp)
+
+    ax = fig.add_subplot(Nr,1,3)
+    ax.cla()
+    ax.set_title("Y, Ytarget")
+    ax.plot(Dp,label = "d",color = "b")
+    ax.plot(Yp,label = "y",color = "r")
+    plt.legend()
     plt.show()
 
-    # グラフ表示用データ
-    T_disp = (-100, 100)
-    t_axis = np.arange(T_disp[0], T_disp[1])
-    disp_U = np.concatenate((train_U[T_disp[0]:], test_U[:T_disp[1]]))
-    disp_D = np.concatenate((train_D[T_disp[0]:], test_D[:T_disp[1]]))
-    disp_Y = np.concatenate((train_Y[T_disp[0]:], test_Y[:T_disp[1]]))
+def calc(Yp,Dp):
+    error = (Yp-Dp)**2
+    NMSE = np.mean(error)/np.var(Dp)
+    RMSE = np.sqrt(np.mean(error))
+    NRMSE = RMSE/np.var(Dp)
+    return RMSE,NRMSE,NMSE
+    
+def execute(c):
+    global D,Ds,Dp,U,Us,Up,Rs,R2s,MM,Yp
+    global RMSE1,RMSE2
 
-    # グラフ表示
-    plt.rcParams['font.size'] = 12
-    fig = plt.figure(figsize=(7, 5))
-    plt.subplots_adjust(hspace=0.3)
+    c.Nh = int(c.Nh)
 
-    ax1 = fig.add_subplot(2, 1, 1)
-    ax1.text(-0.15, 1, '(a)', transform=ax1.transAxes)
-    ax1.text(0.2, 1.05, 'Training', transform=ax1.transAxes)
-    ax1.text(0.7, 1.05, 'Testing', transform=ax1.transAxes)
-    plt.plot(t_axis, disp_U[:,0], color='k')
-    plt.ylabel('Input')
-    plt.axvline(x=0, ymin=0, ymax=1, color='k', linestyle=':')
+    np.random.seed(seed = int(c.seed))    
+    generate_weight_matrix()
 
-    ax2 = fig.add_subplot(2, 1, 2)
-    ax2.text(-0.15, 1, '(b)', transform=ax2.transAxes)
-    plt.plot(t_axis, disp_D[:,0], color='k', label='Target')
-    plt.plot(t_axis, disp_Y[:,0], color='gray', linestyle='--', label='Model')
-    plt.xlabel('n')
-    plt.ylabel('Output')
-    plt.legend(bbox_to_anchor=(1, 1), loc='upper right')
-    plt.axvline(x=0, ymin=0, ymax=1, color='k', linestyle=':')
+    ### generate data
+    
+    if c.dataset==6:
+        rm = 200
+        MM1 = 600
+        MM2 = 500
+        U,D  = generate_narma(N=MM1+MM2+rm,seed=0)
+        U = U[rm:]
+        D = D[rm:]
+        U1 = U[:MM1]
+        U2 = U[MM1:]
+        D1 = D[:MM1]
+        D2 = D[MM1:]
 
-    plt.show()
+    ### training
+    #print("training...")
+    
+    #Scale to (-1,1)
+    c.MM = MM1
+    Dp = D1               # TARGET   #(MM,len(delay))   
+    Up = U1                # INPUT    #(MM,1)
+
+    train_network()
+    #print("...end") 
+    
+    ### test
+    #print("test...")
+    c.MM = MM2
+    Up = U2                # TARGET   #(MM,len(delay))   
+    Dp = D2                # TARGET   #(MM,len(delay))   
+
+    test_network()                  #OUTPUT = Yp
+
+    # Dp = D[MM1:]                # TARGET   #(MM,len(delay))   
+    # Yp = Yp[MM1: ]               # INPUT    #(MM,1)
+    
+    ### evaluation
+
+    RMSE,NRMSE,NMSE = calc(Yp,Dp)
+    #print(1/np.var(Dp))
+    print("RMSE:",RMSE)
+    print("NRMSE:",NRMSE)
+    print("NMSE:",NMSE)
+
+
+
+######################################################################################
+     # Results
+    c.RMSE = RMSE
+    c.NRMSE = NRMSE
+    c.NMSE = NMSE
+#####################################################################################
+
+    if c.plot:
+        plot1()
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-config", type=str)
+    a = ap.parse_args()
+
+    c=Config()
+    if a.config: c=common.load_config(a)
+    execute(c)
+    if a.config: common.save_config(c)
