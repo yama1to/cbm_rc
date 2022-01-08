e6be9df13eb46955747349bab0e1456ada9dc1a5
yama1to <yamatosakino1113@gmail.com>
2022-01-08 18:05:42 +0900

    new

--- deepcbm.py
+++ deepcbm.py
@@ -3,9 +3,12 @@ from numpy.core.numeric import zeros_like
 
 from tqdm import tqdm
 
+global NN
+NN = 2**1
+Temp = 1
+
 # affine変換
 def affine(z, W, b):
-
     return np.dot(z, W) + b
 
 # affine変換勾配
@@ -62,23 +65,38 @@ def decode(u_s,step):
                 #print(i,j,step,i*step+j)
                 if u_s[X,i*step+j] == 1:
                     R = 1
-
             dec[X,i] = 2*fallingTime - 1
     return dec
 
 def cbm(us,Wi,b1,hs,hx,Temp,dt,):
-    z= (2*us-1)@Wi + b1
+    z = (2*us-1)@Wi + b1
     hsign = 1 - 2*hs
-    hx = hx + hsign*(1.0+np.exp(hsign*z/Temp))*dt
+    dx = hsign*(1.0+np.exp(hsign*z/Temp))*dt
+    hx = hx + dx
     hs = np.heaviside(hx+hs-1,0)
-    #hx = np.fmin(np.fmax(hx,0),1)
-    return hs,hx
+    hx = np.fmin(np.fmax(hx,0),1)
+    return hs,hx,dx
 
+def gradient(dx,Temp,hsign,z):
+    print("hsign:",str(hsign.shape))
+    print("z:",str(z.shape))
+    p  = hsign*z/Temp
+    return dx*p
+
+def cbm_back(dx,u,hs,W,Temp):
+    hsign = 1-2*hs
+
+    z = 2*u-1
+    dW = gradient(dx,Temp,hsign,z)
+    z = 2*W
+    du = gradient(dx,Temp,hsign,z)
+    z = np.ones(hs.shape[0])
+    db = gradient(dx,Temp,hsign,z)
+    return du, dW, db
 
 def learn(batch_size,nx_train, t_train, W1, b1, W2, b2, W3, b3, lr):
-    NN = 256
-    Temp = 1
     dt = 1/NN
+
     hs1 = np.zeros((100))
     hs2 = np.zeros((50))
     hs3 = np.zeros((10))
@@ -87,7 +105,7 @@ def learn(batch_size,nx_train, t_train, W1, b1, W2, b2, W3, b3, lr):
     hx2 = np.zeros((50))
     hx3 = np.zeros((10))
     
-    Z = np.zeros((100*NN,10))
+
     m = 0
     y = np.zeros((nx_train.shape[0],10))
     rs =1
@@ -102,13 +120,102 @@ def learn(batch_size,nx_train, t_train, W1, b1, W2, b2, W3, b3, lr):
         theta = np.mod(m/NN,1) # (0,1)
         rs = p2s(theta,0)# 参照クロック
         us = p2s(theta,x)
+
+        # 順伝播
+        hs1,hx1,dx1 = cbm(us,W1,b1,hs1,hx1,Temp,dt,)
+        z1 = hs1
+        hs2,hx2,dx2 = cbm(z1,W2,b2,hs2,hx2,Temp,dt,)
+        z2 = hs2
+        hs3,hx3,dx3 = cbm(z2,W3,b3,hs3,hx3,Temp,dt,)
+        z3 = softmax(hs3[:,np.newaxis])
+ 
+        hs = z3[:,0]
+        hc[(hs_prev == 1)& (hs==0)] = count
+    
+        # ref.clockの立ち上がり
+        if rs_prev==0 and rs==1:
+            yp = 2*hc/NN-1 # デコード、カウンタの値を連続値に変換
+            hc = np.zeros(10) #カウンタをリセット
+            #ht = 2*hs-1 #リファレンスクロック同期用ラッチ動作をコメントアウト
+            y[m]=yp
+            count = 0
+            m += 1
+
+            #back propagation
+            dy = softmax_cross_entropy_error_back(y, t)
+            du2, dW3, db3 = cbm_back(dx3,dy ,hs3, W3, b3) #cbm_back(dx,u,W,hsign,Temp)
+            du1, dW2, db2 = cbm_back(dx2,du2,hs2, W2, b2)
+            _ , dW1, db1 = cbm_back(dx1,du1,hs1,W1, b1)
+            # du1, dW1, db1 = cbm_back(dx1,dy,hs1,W1, b1)
+            # du2, dW2, db2 = cbm_back(dx2,du1,hs2, W2, b2)
+            # _, dW3, db3 = cbm_back(dx3,du2 ,hs3, W3, b3) #cbm_back(dx,u,W,hsign,Temp)
+           
+            # 重み、バイアスの更新
+            W1 = W1 - lr * dW1
+            b1 = b1 - lr * db1
+            W2 = W2 - lr * dW2
+            b2 = b2 - lr * db2
+            W3 = W3 - lr * dW3
+            b3 = b3 - lr * db3
+
+        #境界条件
+        if k != (NN*nx_train.shape[0]-1):
+            yp = 2*hc/NN-1 # デコード、カウンタの値を連続値に変換
+            y[m]=yp
+
+            #back propagation 
+            dy = softmax_cross_entropy_error_back(y, t)
+            du2, dW3, db3 = cbm_back(dx3,dy ,hs3, W3, b3) #cbm_back(dx,u,W,hsign,Temp)
+            du1, dW2, db2 = cbm_back(dx2,du2,hs2, W2, b2)
+            _   , dW1, db1 = cbm_back(dx1,du1,hs1,W1, b1)
+            # du1, dW1, db1 = cbm_back(dx1,dy,hs1,W1, b1)
+            # du2, dW2, db2 = cbm_back(dx2,du1,hs2, W2, b2)
+            # _, dW3, db3 = cbm_back(dx3,du2 ,hs3, W3, b3) #cbm_back(dx,u,W,hsign,Temp)
+
+            # 重み、バイアスの更新
+            W1 = W1 - lr * dW1
+            b1 = b1 - lr * db1
+            W2 = W2 - lr * dW2
+            b2 = b2 - lr * db2
+            W3 = W3 - lr * dW3
+            b3 = b3 - lr * db3
+        count += 1
+    return W1, b1, W2, b2, W3, b3
+
+def predict(x, W1, b1, W2, b2, W3, b3):
+    dt = 1/NN
+
+    hs1 = np.zeros((100))
+    hs2 = np.zeros((50))
+    hs3 = np.zeros((10))
+    
+    hx1 = np.zeros((100))
+    hx2 = np.zeros((50))
+    hx3 = np.zeros((10))
+    
+    Z = np.zeros((100*NN,10))
+    m = 0
+    y = np.zeros((x.shape[0],10))
+    rs =1
+    count = 0
+    hc = np.zeros((10))
+    hs = np.zeros(10)
+
+    for k in tqdm(range(0,NN*x.shape[0])):
+        idx = int(k/NN)
+        x,t = nx_train[idx], t_train[idx]
+        hs_prev = hs.copy()
+        rs_prev = rs
+        theta = np.mod(m/NN,1) # (0,1)
+        rs = p2s(theta,0)# 参照クロック
+        us = p2s(theta,x)
         # 順伝播
         #print(us.shape,W1.shape)
-        u1,hx1 = cbm(us,W1,b1,hs1,hx1,Temp,dt,)
+        u1,hx1,_ = cbm(us,W1,b1,hs1,hx1,Temp,dt,)
         z1 = u1
-        u2,hx2 = cbm(z1,W2,b2,hs2,hx2,Temp,dt,)
+        u2,hx2,_ = cbm(z1,W2,b2,hs2,hx2,Temp,dt,)
         z2 = u2
-        u3,hx3 = cbm(z2,W3,b3,hs3,hx3,Temp,dt,)
+        u3,hx3,_ = cbm(z2,W3,b3,hs3,hx3,Temp,dt,)
         z3 = softmax(u3[:,np.newaxis])
         #print(z3.shape)
         hs = z3[:,0]
@@ -116,49 +223,22 @@ def learn(batch_size,nx_train, t_train, W1, b1, W2, b2, W3, b3, lr):
         hc[(hs_prev == 1)& (hs==0)] = count
     
         # ref.clockの立ち上がり
+
         if rs_prev==0 and rs==1:
-            hp = 2*hc/NN-1 # デコード、カウンタの値を連続値に変換
+            yp = 2*hc/NN-1 # デコード、カウンタの値を連続値に変換
             hc = np.zeros(10) #カウンタをリセット
             #ht = 2*hs-1 #リファレンスクロック同期用ラッチ動作をコメントアウト
-            y[m]=hp
+            y[m]=yp
             count = 0
             m += 1
-
         #境界条件
-        if k == (NN * batch_size-1):
-            hp = 2*hc/NN-1 # デコード、カウンタの値を連続値に変換
-            y[m]=hp
+        if k == (NN*nx_train.shape[0]-1):
+            yp = 2*hc/NN-1 # デコード、カウンタの値を連続値に変換
+            y[m]=yp
         count += 1
-
-
-    for i in range(nx_train.shape[0]):
-        # 逆伝播
-        dy = softmax_cross_entropy_error_back(y, t)
-        dz2, dW3, db3 = affine_back(dy, z2, W3, b3)
-        du2 = relu_back(dz2, u2)
-        dz1, dW2, db2 = affine_back(du2, z1, W2, b2)
-        du1 = relu_back(dz1, u1)
-        dx, dW1, db1 = affine_back(du1, x, W1, b1)
-        # 重み、バイアスの更新
-        W1 = W1 - lr * dW1
-        b1 = b1 - lr * db1
-        W2 = W2 - lr * dW2
-        b2 = b2 - lr * db2
-        W3 = W3 - lr * dW3
-        b3 = b3 - lr * db3
-
-
-    return W1, b1, W2, b2, W3, b3
-
-def predict(x, W1, b1, W2, b2, W3, b3):
-    # 順伝播
-    u1 = affine(x, W1, b1)
-    z1 = relu(u1)
-    u2 = affine(z1, W2, b2)
-    z2 = relu(u2)
-    u3 = affine(z2, W3, b3)
-    y  = softmax(u3)
     return y
+
+    
 
 
 import gzip
@@ -195,6 +275,8 @@ def _load_label( label_path ) :
 
 # 正解率
 def accuracy_rate(y, t):
+    print(y)
+    print(t)
     max_y = np.argmax(y, axis=1)
     max_t = np.argmax(t, axis=1)
     return np.sum(max_y == max_t)/y.shape[0]
@@ -208,7 +290,9 @@ if __name__ == "__main__":
     # 入力データの正規化(0～1)
     nx_train = x_train/255
     nx_test  = x_test/255
-
+    #print(x_train.shape, t_train.shape, x_test.shape, t_test.shape )
+    # = (60000, 784) (60000, 10) (10000, 784) (10000, 10)
+    #print(x_train.shape, t_train.shape, x_test.shape, t_test.shape )
     # ノード数設定
     d0 = nx_train.shape[1]
     d1 = 100 # 1層目のノード数
@@ -227,15 +311,19 @@ if __name__ == "__main__":
     # 学習率
     lr = 0.5
     # バッチサイズ
-    batch_size = 100
+    batch_size =100
     # 学習回数
     epoch = 50
-
+    
     # 予測（学習データ）
     y_train = predict(nx_train, W1, b1, W2, b2, W3, b3)
     # 予測（テストデータ）
     y_test = predict(nx_test, W1, b1, W2, b2, W3, b3)
     # 正解率、誤差表示
+    #print(x_train.shape, t_train.shape, x_test.shape, t_test.shape )
+    # = (60000, 784) (60000, 10) (10000, 784) (10000, 10)
+    #print(y_train.shape, t_train.shape, y_test.shape, t_test.shape )
+    # #(60000, 10) (60000, 10) (60000, 10) (10000, 10)
     train_rate, train_err = accuracy_rate(y_train, t_train), cross_entropy_error(y_train, t_train)
     test_rate, test_err = accuracy_rate(y_test, t_test), cross_entropy_error(y_test, t_test)
     print("{0:3d} train_rate={1:6.2f}% test_rate={2:6.2f}% train_err={3:8.5f} test_err={4:8.5f}".format((0), train_rate*100, test_rate*100, train_err, test_err))
@@ -244,13 +332,12 @@ if __name__ == "__main__":
         # 学習
         W1, b1, W2, b2, W3, b3 = learn(batch_size,nx_train, t_train, W1, b1, W2, b2, W3, b3, lr)
         
-        
-
         # 予測（学習データ）
         y_train = predict(nx_train, W1, b1, W2, b2, W3, b3)
         # 予測（テストデータ）
         y_test = predict(nx_test, W1, b1, W2, b2, W3, b3)
         # 正解率、誤差表示
+        
         train_rate, train_err = accuracy_rate(y_train, t_train), cross_entropy_error(y_train, t_train)
         test_rate, test_err = accuracy_rate(y_test, t_test), cross_entropy_error(y_test, t_test)
         print("{0:3d} train_rate={1:6.2f}% test_rate={2:6.2f}% train_err={3:8.5f} test_err={4:8.5f}".format((i+1), train_rate*100, test_rate*100, train_err, test_err))
